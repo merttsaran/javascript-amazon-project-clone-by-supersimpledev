c9c3f88bfc70cc78ae89aca54de77c4251e10ca1
merttsaran <merttsaran@gmail.com>
2026-04-02 15:47:16 +0300

    feat(cart): enable update and save buttons for quantity editing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -39,8 +39,8 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
             </span>
-            <input class="quantity-input"
-            <span class="save-quantity-link link-primary">Save</span>
+            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+            <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -126,3 +126,20 @@ document.querySelectorAll('.js-update-link').forEach((link) => {
     container.classList.add('is-editing-quantity');
   });
 });
+
+document.querySelectorAll('.js-save-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.remove('is-editing-quantity');
+
+    const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+    const newQuantity = Number(quantityInput.value);
+    updateQuantity(productId, newQuantity);
+
+  });
+});
+
+
+
