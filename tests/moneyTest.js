10ea1fa68ef7ae432064f318e7c1ac046620ae96
merttsaran <merttsaran@gmail.com>
2026-04-13 23:54:28 +0300

    test: extend formatCurrency tests with rounding cases

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,5 +1,7 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
+console.log('converts cents into dollars');
+
 if (formatCurrency(2095) === '20.95') {
   console.log('passed');
 } else {
@@ -10,4 +12,16 @@ if (formatCurrency(0) === '0.00') {
   console.log('passed');
 } else {
   console.log('failed');
+}
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed')
+} else {
+  console.log('failed')
+}
+
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('passed')
+} else {
+  console.log('failed')
 }
