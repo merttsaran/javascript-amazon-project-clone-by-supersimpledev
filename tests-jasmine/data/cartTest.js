f05c6b9d75aa8bf33bed34502a947158b3f4f01a
merttsaran <merttsaran@gmail.com>
2026-04-17 00:03:41 +0300

    test: expand addToCart test to verify cart item and storage

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -6,6 +6,8 @@ describe('test suite: addToCart', () => {
   });
 
   it('adds a new product to the cart', () => {
+    spyOn(localStorage, 'setItem');
+
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
@@ -13,5 +15,8 @@ describe('test suite: addToCart', () => {
 
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
   })
 });
