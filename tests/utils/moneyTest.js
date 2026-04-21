bef4df809b036ae48bfa732d6a46050f55d74b4f
merttsaran <merttsaran@gmail.com>
2026-04-22 00:52:08 +0300

    refactor: clean up tests using beforeEach, afterEach and toHaveBeenCalledWith

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -12,4 +12,10 @@ describe('test suite: formatCurrency', () => {
   it('rounds up to nearest cent', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
   });
+  it('rounds up to nearest cent', () => {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+  it('works with negative numbers', () => {
+    expect(formatCurrency(-500)).toEqual('-5.00');
+  })
 });
