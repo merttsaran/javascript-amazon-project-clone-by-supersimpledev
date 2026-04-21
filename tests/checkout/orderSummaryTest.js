1727d70b072df5e5a24afd1827a8eef750d157ac
merttsaran <merttsaran@gmail.com>
2026-04-22 01:05:08 +0300

    test: verify product names are displayed correctly in cart

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -44,6 +44,12 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
   });
 
   it('removes a product', () => {
@@ -57,6 +63,9 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
     ).not.toEqual(null);
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
   });
