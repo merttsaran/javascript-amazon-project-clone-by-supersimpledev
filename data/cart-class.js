c7094bc85b0106bc06aad558ed48007ce8c911b7
Mert Saran <merttsaran@gmail.com>
2026-05-11 23:17:37 +0300

    refactor: create class version of cart

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,6 +1,11 @@
 class Cart{
-    cartItems = undefined;
-    localStorageKey = undefined;
+    cartItems;
+    localStorageKey;
+
+    constructor(localStorageKey) {
+      this.localStorageKey = localStorageKey;
+      this.loadFromStorage();
+    }
 
     loadFromStorage() {
       this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)); 
@@ -107,14 +112,8 @@ class Cart{
     }
 }
 
-const cart = new Cart();
-const businessCart = new Cart();
-
-cart.localStorageKey = 'cart-oop';
-businessCart.localStorageKey = 'cart-business';
-
-cart.loadFromStorage();
-businessCart.loadFromStorage();
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
 
 console.log(cart);
 console.log(businessCart);
