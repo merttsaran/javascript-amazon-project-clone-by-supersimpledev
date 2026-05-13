9b29eba4beb33d9a226f53fb0fd61cfbbb1ef25a
Mert Saran <merttsaran@gmail.com>
2026-05-14 02:04:51 +0300

    chore: add trunk open/close functionality to Car class exercise

--- data/car.js
+++ data/car.js
@@ -2,6 +2,7 @@ class Car {
     brand;
     model;
     speed = 0;
+    isTrunkOpen = false;
 
     constructor(carDetails) {
         this.brand = carDetails.brand;
@@ -9,11 +10,15 @@ class Car {
     }
 
     displayInfo() {
-        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h`);
+        const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
+        console.log(`${this.brand} ${this.model}, Speed: ${this.speed} km/h, Trunk: ${trunkStatus}`);
     }
 
     go() {
-        this.speed += 5;
+        if (!this.isTrunkOpen) {
+            this.speed += 5;
+        }
 
         if (this.speed > 200) {
             this.speed = 200;
@@ -26,6 +31,16 @@ class Car {
         if(this.speed < 0) {
             this.speed = 0;
         }
+    }
+
+    openTrunk() {
+        if (this.speed === 0) {
+            this.isTrunkOpen = true;
+        }
+    }
+
+    closeTrunk() {
+        this.isTrunkOpen = false;
     }
 }
 
@@ -48,8 +63,16 @@ car1.go();
 car1.go();
 car1.brake();
 car1.displayInfo();
+
+car1.openTrunk();
+car1.displayInfo();
+
 car2.displayInfo();
 car2.go();
 car2.brake();
 car2.brake();
+car2.displayInfo();
+
+car2.openTrunk();
+car2.go();
 car2.displayInfo();
