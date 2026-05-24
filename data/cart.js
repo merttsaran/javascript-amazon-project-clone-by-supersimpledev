9f8075d4c2160826585b16cb59669f92f73953c1
Mert Saran <merttsaran@gmail.com>
2026-05-24 22:17:50 +0300

    feat: chain loadCart Promise before rendering order and payment summary

--- data/cart.js
+++ data/cart.js
@@ -106,3 +106,14 @@ export function updateDeliveryOption(productId, newDeliveryOptionId) {
   saveToStorage();
 }
 
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
 //import '../data/cart-class.js';
 import '../data/car.js';
 // import '../data/backend-practice.js';
@@ -9,6 +10,14 @@ new Promise((resolve) => {
   loadProducts(() => {
     resolve();
   });
+
+}).then(() => {
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+  
 }).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
