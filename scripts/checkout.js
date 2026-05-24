4f9abfba75e08c539c33380fe5da5881bb9045a9
Mert Saran <merttsaran@gmail.com>
2026-05-24 22:24:27 +0300

    refactor: use Promise.all to load products and cart in parallel

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,6 +6,24 @@ import {loadCart} from '../data/cart.js';
 import '../data/car.js';
 // import '../data/backend-practice.js';
 
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve();
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+   
+]).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+
+/*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve();
@@ -17,11 +35,12 @@ new Promise((resolve) => {
       resolve();
     });
   });
-  
+
 }).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 loadProducts(() => {
