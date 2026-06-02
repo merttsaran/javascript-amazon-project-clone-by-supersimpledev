f60f0039d40450a587fa78b8742ba9e2471fe146
Mert Saran <merttsaran@gmail.com>
2026-06-02 17:05:23 +0300

    refactor: convert Promise chain to async/await with loadPage function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,26 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
 import {loadCart} from '../data/cart.js';
-//import '../data/cart-class.js';
-import '../data/car.js';
+// import '../data/cart-class.js';
+// import '../data/car.js';
 // import '../data/backend-practice.js';
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -18,6 +34,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
