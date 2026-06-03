40d06157036a9d8692d9368bee4e3ba28f4bdbab
Mert Saran <merttsaran@gmail.com>
2026-06-03 12:15:32 +0300

    chore: learn throw and reject

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -8,14 +8,18 @@ import {loadCart} from '../data/cart.js';
 
 async function loadPage() {
   try {
+    // throw 'error1';
+
     await loadProductsFetch();
 
-    const value = await new Promise((resolve) => {
+    const value = await new Promise((resolve, reject) => {
+      // throw 'error2';
       loadCart(() => {
+        // reject('error3');
         resolve('value3');
       });
     });
-    
+
   } catch (error) {
     console.log('Unexpected error. Please try again later.');
   };
