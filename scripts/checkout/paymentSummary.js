92a2549678e8f02e6451d229c018bdfc0330fdfa
Mert Saran <merttsaran@gmail.com>
2026-06-04 10:53:40 +0300

    feat: send cart data to backend API on place order button click

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -58,10 +58,28 @@ export function renderPaymentSummary() {
         $${formatCurrency(totalCents)}</div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary
+     js-place-order">
       Place your order
     </button>
   `;
 
-  document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+  document.querySelector('.js-payment-summary')
+    .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+
+      const order = await response.json();
+      console.log(order);
+    });
 }
